9ec95208290dad24bf2e1b7e35cbccb8748ec370
EmanuelVini <vinicius.emanuel.cg@hotmail.com>
2024-11-27 14:56:55 -0300

    2 commit

--- app.js
+++ app.js
@@ -12,15 +12,42 @@ function sortear() {
 
     for (let i = 0; i < quantidade; i++){
         numero = obterNumeroAleatorio(inicio, fim);
+
+        while(sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(inicio, fim);
+        }
+            
         sorteados.push(numero);
 
     }
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+
+    alterarStatusBotao()
     //let numero = obterNumeroAleatorio(inicio, fim);
     //alert(numero);
 }
 
 function obterNumeroAleatorio(min , max) {
     return Math.floor(Math.random() *(max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarStatusBotao();
 }
